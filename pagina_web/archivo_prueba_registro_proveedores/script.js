8dec1a86b86ae98bce9e3b090a3b56e4072cb100
Yossec SA <yosec.arratea22@gmail.com>
2023-05-28 11:33:58 -0500

    carpeta con el diseño de registro proveedores y consumo de la API

---
+++ pagina_web/archivo_prueba_registro_proveedores/script.js
@@ -0,0 +1,33 @@
+document.getElementById("proveedorForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+  
+    var proveedorData = {
+      nombre: document.getElementById("nombre").value,
+      direccion: document.getElementById("direccion").value,
+      celular: document.getElementById("celular").value,
+      tipo_proveedor: document.getElementById("tipo_proveedor").value,
+      red_social: document.getElementById("red_social").value
+    };
+  
+    // Enviar los datos a la API mediante una solicitud AJAX
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://192.168.0.15/pagina_web/registro_proveedores.php", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          var response = JSON.parse(xhr.responseText);
+          if (response.status === 'success') {
+            alert("Proveedor guardado correctamente.");
+            // Restablecer el formulario
+            document.getElementById("proveedorForm").reset();
+          } else {
+            alert("Error al guardar el proveedor: " + response.message);
+          }
+        } else {
+          alert("Error en la solicitud AJAX.");
+        }
+      }
+    };
+    xhr.send(JSON.stringify(proveedorData));
+  });
