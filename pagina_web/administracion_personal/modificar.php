92d7c380ba284fec7b475e61b5c030352134a4ba
TFstorm22 <1376280@senati.pe>
2023-05-29 02:16:32 -0500

    modificar personal

---
+++ pagina_web/administracion_personal/modificar.php
@@ -0,0 +1,84 @@
+<?php
+	require 'conexion.php';
+	
+	$id = $_GET['id'];
+	
+	$sql = "SELECT * FROM personal WHERE id = '$id'";
+	$resultado = $conn->query($sql);
+	$row = $resultado->fetch_array(MYSQLI_ASSOC);
+	
+?>
+<html lang="es">
+	<head>
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/bootstrap-theme.css" rel="stylesheet">
+		<script src="js/jquery-3.1.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>	
+	</head>
+	
+	<body>
+		<div class="container">
+			<div class="row">
+				<h3 style="text-align:center">MODIFICAR REGISTRO</h3>
+			</div>
+			
+			<form class="form-horizontal" method="POST" action="update.php" autocomplete="off">
+				<div class="form-group">
+					<label for="nombre" class="col-sm-2 control-label">Nombre y Apellidos</label>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre" value="<?php echo $row['nombre_apellido']; ?>" required>
+					</div>
+				</div>
+				
+				<input type="hidden" id="id" name="id" value="<?php echo $row['id']; ?>" />
+				
+				<div class="form-group">
+					<label for="dni" class="col-sm-2 control-label">DNI</label>
+					<div class="col-sm-10">
+						<input type="dni" class="form-control" id="dni" name="dni" placeholder="DNI" value="<?php echo $row['dni']; ?>"  required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="foto" class="col-sm-2 control-label">Foto</label>
+					<div class="col-sm-10">
+						<input type="foto" class="form-control" id="foto" name="foto" placeholder="Foto" value="<?php echo $row['foto']; ?>"  required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="cargo" class="col-sm-2 control-label">Cargo</label>
+					<div class="col-sm-10">
+						<input type="cargo" class="form-control" id="cargo" name="cargo" placeholder="Cargo" value="<?php echo $row['id_cargo']; ?>"  required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="contra" class="col-sm-2 control-label">Contraseña</label>
+					<div class="col-sm-10">
+						<input type="contra" class="form-control" id="contra" name="contra" placeholder="Contraseña" value="<?php echo $row['contrasena']; ?>" required >
+					</div>
+				</div>
+				
+				<div class="form-group">
+					<label for="estado" class="col-sm-2 control-label">Estado</label>
+					<div class="col-sm-10">
+						<select class="form-control" id="estado" name="estado" required>
+							<option value="1" <?php if($row['estado_personal']=='1') echo 'selected'; ?>>ACTIVO</option>
+							<option value="0" <?php if($row['estado_personal']=='0') echo 'selected'; ?>>INACTIVO</option>
+							
+						</select>
+					</div>
+				</div>
+				
+				
+						
+				<div class="form-group">
+					<div class="col-sm-offset-2 col-sm-10">
+						<a href="index.php" class="btn btn-default">Regresar</a>
+						<button type="submit" class="btn btn-primary">Guardar</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</body>
+</html>
