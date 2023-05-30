b45fec6fadf1884bc633b3b6a77e32d695386ede
Driner14 <115844334+Driner14@users.noreply.github.com>
2023-05-30 15:54:29 -0500

    Formulario de registrar produtos acabado.

---
+++ pagina_web/registro/registroProducto.js
@@ -0,0 +1,42 @@
+function driner(){
+    var proveedorData = {
+    descripcion: document.getElementById("descripcion").value,
+      precio: document.getElementById("precio").value,
+      stock: document.getElementById("stock").value,
+      codigo_producto: document.getElementById("codigo_producto").value,
+      id_marca: document.getElementById("id_marca").value,
+      id_familia: document.getElementById("id_familia").value,
+      nombre: document.getElementById("nombre").value,
+      id_ubicacion: document.getElementById("id_ubicacion").value,
+      estado_producto: document.getElementById("estado_producto").value,
+      id_proveedor: document.getElementById("id_proveedor").value,
+      imagenes: document.getElementById("file").value.substr(11)
+
+
+    };
+
+    // Enviar los datos a la API mediante una solicitud AJAX
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST","http://localhost/registro/registro_producto.php", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          var response = JSON.parse(xhr.responseText);
+          if (response.status === 'success') {
+            console.log("Productos guardado correctamente.");
+            
+            
+            // Restablecer el formulario
+            
+            document.getElementById("registroProductos").reset();
+          } else {
+            console.log("Error al guardar el proveedor: " + response.message);
+          }
+        } else {
+          console.log("Error en la solicitud AJAX.");
+        }
+      }
+    };
+    xhr.send(JSON.stringify(proveedorData));
+  }
