cfa058bcf025cf893b03ff8a3625334da0997088
Driner14 <tuctonieto@gmail.com>
2023-06-07 20:17:03 -0500

    Agregando diseños al pantalla registro

---
+++ movil/RegistroProductos/app/src/main/java/com/example/myapplication/WebService.kt
@@ -0,0 +1,19 @@
+package com.example.uploadimage
+
+import retrofit2.Response
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface WebService {
+
+    @FormUrlEncoded
+    //COPIAMIOS LA RUTA
+    @POST("upload_image.php")
+    suspend fun enviarImage(
+        @Field("idImage") idImage: String,
+        @Field("nomImage") nomImage: String,
+        @Field("image") image: String
+    ): Response<String>
+
+}
---
+++ movil/RegistroProductos/app/src/main/java/com/example/myapplication/constantes.kt
@@ -0,0 +1,6 @@
+package com.example.uploadimage
+
+object Constantes {
+    const val BASE_URL = "http://192.168.100.17/upload_image/"
+
+}
